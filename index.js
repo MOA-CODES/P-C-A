60e67acc29c353bf48a7a3bc19f27c76f5fab68c
MOA -CODES <moyo.ajla@gmail.com>
2024-02-19 18:07:54 +0100

    progress

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 require('dotenv').config()
 require('express-async-errors')
 
+const conn = require('./db/conn')
+const sequelize = require('sequelize')
 const express = require('express')
 const app = express()
+
+const nf = require('./middleware/not-found')
+
+
+
+app.get('/api/home', (req, res)=>{
+    res.json({info: 'Node.js, Express, and Postgres API'})
+    // .send('Postgres Crud app')
+})
+
+app.use(express.json())
+app.use(nf)
+
+app.listen(8000, ()=>{
+    console.log('listening on port 8000')
+})
+
 
---
+++ middleware/not-found.js
@@ -0,0 +1,3 @@
+const notfound = (req, res)=>res.status(404).send('directory does not exist attach /api/home to go to the home page')
+
+module.exports = notfound
